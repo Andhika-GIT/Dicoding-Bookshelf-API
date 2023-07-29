387ea164bdfc503fafebe18d7d07dbcf5d90561c
Andhika Prasetya <andhikapr081@gmail.com>
2023-07-29 11:53:53 +0700

    update server.js

--- src/server.js
+++ src/server.js
@@ -5,17 +5,12 @@ const init = async () => {
   const server = Hapi.server({
     port: 9000,
     host: "localhost",
-    routes: {
-      cors: {
-        origin: ["*"],
-      },
-    },
   });
 
   server.route(routes);
 
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server sedang berjalan pada port ${server.info.port}`);
 };
 
 init();
